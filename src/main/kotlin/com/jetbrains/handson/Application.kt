8aa393f2d8d97ddf303b738e00fea66f454fc279
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2021-03-31 18:17:55 +0200

    Creating blog entries logic

--- src/main/kotlin/com/jetbrains/handson/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/Application.kt
@@ -1,8 +1,14 @@
 package com.jetbrains.handson
 
+import com.jetbrains.handson.model.BlogEntry
+import com.jetbrains.handson.model.blogEntries
 import freemarker.cache.ClassTemplateLoader
+import freemarker.core.HTMLOutputFormat
 import io.ktor.application.*
 import io.ktor.freemarker.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
@@ -10,14 +16,25 @@ fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        outputFormat = HTMLOutputFormat.INSTANCE
     }
     routing {
         get("/") {
-            call.respond("Hello, Ktor!")
+            call.respond(FreeMarkerContent("index.ftl", mapOf("entries" to blogEntries)))
+        }
+        static("/static") {
+            resources("files")
+        }
+        post("/submit") {
+            val params = call.receiveParameters()
+            val headline = params["headline"] ?: return@post call.respond(HttpStatusCode.BadRequest)
+            val body = params["body"] ?: return@post call.respond(HttpStatusCode.BadRequest)
+            val newEntry = BlogEntry(headline, body)
+            blogEntries.add(0, newEntry)
+            call.respondRedirect("/")
         }
     }
 }
