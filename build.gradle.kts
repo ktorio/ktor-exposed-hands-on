cdb18708b1350529f9d0fc85529c514218a768e6
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2021-05-11 15:30:02 +0200

    Added external dependencies; updated versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
+val ktor_version = "1.6.4"
+val kotlin_version = "1.5.31"
+val logback_version = "1.2.6"
+val exposed_version = "0.35.2"
+val h2_version = "1.4.200"
+val hikaricp_version = "5.0.0"
+val postgresql_version = "42.3.0"
+val ehcache_version = "3.9.7"
 
 plugins {
     application
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.5.31"
 }
 
 group = "com.jetbrains.handson"
@@ -23,4 +28,12 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("com.h2database:h2:$h2_version")
+    implementation("org.postgresql:postgresql:$postgresql_version")
+    implementation("com.zaxxer:HikariCP:$hikaricp_version")
+    implementation("org.ehcache:ehcache:$ehcache_version")
 }
