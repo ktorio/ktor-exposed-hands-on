867bb0a9c08bb9d3d535a1523d38181901a9c712
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2021-06-21 13:23:00 +0200

    Added connection pooling with HikariCP

--- src/main/kotlin/com/jetbrains/handson/dao/DatabaseFactory.kt
+++ src/main/kotlin/com/jetbrains/handson/dao/DatabaseFactory.kt
@@ -1,6 +1,8 @@
 package com.jetbrains.handson.dao
 
 import com.jetbrains.handson.model.BlogEntries
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import io.ktor.config.*
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
@@ -17,11 +19,23 @@ object DatabaseFactory {
                 (config.propertyOrNull("storage.dbFilePath")?.getString()?.let {
                     File(it).canonicalFile.absolutePath
                 } ?: "")
-        Database.connect(jdbcURL, driverClassName)
+        Database.connect(createHikariDataSource(url = jdbcURL, driver = driverClassName))
         transaction {
             SchemaUtils.create(BlogEntries)
         }
     }
+
+    private fun createHikariDataSource(
+        url: String,
+        driver: String
+    ) = HikariDataSource(HikariConfig().apply {
+        driverClassName = driver
+        jdbcUrl = url
+        maximumPoolSize = 3
+        isAutoCommit = false
+        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        validate()
+    })
 
     suspend fun <T> dbQuery(block: () -> T): T =
         newSuspendedTransaction(Dispatchers.IO) { block() }
